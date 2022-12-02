49e7a3660357532c6f523586c4bce3b9d0882011
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2022-12-02 16:34:03 -0500

    Ruby Solution to Day1 problem of Calories Counting

---
+++ 2022/ruby/day01/calorie_counting_solution.rb
@@ -0,0 +1,5 @@
+calories = input = File.read("calorie_counting_input.txt").split("\n\n").map{|cal| cal.split("\n").map(&:to_i).sum}
+max_calorie = calories.max
+puts "Max calorie carried by Elf - #{max_calorie}"
+total_top_three_calories = calories.sort.reverse[0..2].sum
+puts "Sum of total calories carried by top three Elf - #{total_top_three_calories}"
