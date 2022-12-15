1e0a914f10365a95f818f3c984e797e89d601bbf
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2022-12-14 22:55:03 -0500

    Day 4 Camp Cleanup  Part 1 solution

---
+++ 2022/ruby/day04/camp_cleanup_solution.rb
@@ -0,0 +1,15 @@
+sections = File.read("camp_cleanup_input.txt").split("\n").freeze
+total_match = 0
+
+def check_match(section)
+  part1, part2 = section.split(",")
+  part1_first, part1_second = part1.split("-").map(&:to_i)
+  part2_first, part2_second = part2.split("-").map(&:to_i)
+  ((part1_first..part1_second).cover?(part2_first..part2_second) || (part2_first..part2_second).cover?(part1_first..part1_second)) ? 1 : 0
+end
+
+sections.each do |section|
+  total_match += check_match(section)
+end
+
+puts "Total number of sections matching are #{total_match}"
