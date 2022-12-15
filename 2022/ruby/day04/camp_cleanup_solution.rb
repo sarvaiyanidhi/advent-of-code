8ab8df3da25ee8ef398eeed7f45623b057249385
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2022-12-14 23:23:15 -0500

    Day 4 Camp Cleanup  Part 2 solution

--- 2022/ruby/day04/camp_cleanup_solution.rb
+++ 2022/ruby/day04/camp_cleanup_solution.rb
@@ -1,5 +1,6 @@
 sections = File.read("camp_cleanup_input.txt").split("\n").freeze
 total_match = 0
+total_overlap = 0
 
 def check_match(section)
   part1, part2 = section.split(",")
@@ -13,3 +14,16 @@ sections.each do |section|
 end
 
 puts "Total number of sections matching are #{total_match}"
+
+def check_overlap(section)
+  part1, part2 = section.split(",")
+  part1_first, part1_second = part1.split("-").map(&:to_i)
+  part2_first, part2_second = part2.split("-").map(&:to_i)
+  ((part1_first..part1_second).begin <= (part2_first..part2_second).end && (part1_first..part1_second).end >= (part2_first..part2_second).begin) ? 1 : 0
+end
+
+sections.each do |section|
+  total_overlap += check_overlap(section)
+end
+
+puts "Total number of sections overlap are #{total_overlap}"
